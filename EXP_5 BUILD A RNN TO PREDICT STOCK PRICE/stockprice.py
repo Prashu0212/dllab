9b081425ae41ee89dfaad57ad806e8481f4ee49d
raya2130 <ksram2103@gmail.com>
2024-01-11 10:03:32 +0530

    Update stockprice.py

--- EXP_5 BUILD A RNN TO PREDICT STOCK PRICE/stockprice.py
+++ EXP_5 BUILD A RNN TO PREDICT STOCK PRICE/stockprice.py
@@ -1,87 +1,38 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
-import math
-
-df = pd.read_csv(r'/content/drive/MyDrive/dl lab/Tesla.csv')
-df.info()
-df['Date'] = pd.to_datetime(df['Date'])
-df.set_index('Date',inplace=True)
-print(df.shape )
-print(df.columns)
-
-plt.figure(figsize=(16,8))
-plt.title('Close Price History')
-plt.plot(df['Close'], color='red')
-plt.xlabel('Date', fontsize=18)
-plt.ylabel('Close Price USD', fontsize = 18)
-plt.show()
-
-data = df.filter(['Close'])
-dataset = data.values
-training_data_len = math.ceil(len(dataset)*.8)
-print(training_data_len)
-
-scaler = MinMaxScaler(feature_range=(0,1))
-scaled_data = scaler.fit_transform(dataset)
-print(scaled_data)
-
-train_data = scaled_data[0:training_data_len, :]
-
-x_train = []
-y_train = []
-for i in range(60,len(train_data)):
-    x_train.append(train_data[i-60:i, 0])
-    y_train.append(train_data[i,0])
-
 import numpy as np
-x_train,y_train = np.array(x_train), np.array(y_train)
-x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
-print(x_train.shape)
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import SimpleRNN, Dense
 
-model =Sequential()
-model.add(LSTM(64,return_sequences=True, input_shape=(x_train.shape[1],1)))
-model.add(LSTM(64, return_sequences= False))
-model.add(Dense(32))
-model.add(Dense(1))
+data = pd.read_csv(r"C:\Users\HP\Downloads\data.csv")
+data['Date'] = pd.to_datetime(data['Date'])
+data.set_index('Date', inplace=True)
 
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+
+sequence_length = 10 
+x, y = [], []
+for i in range(len(scaled_data) - sequence_length):
+ x.append(scaled_data[i:i + sequence_length, 0])
+ y.append(scaled_data[i + sequence_length, 0])
+x, y = np.array(x), np.array(y)
+x = np.reshape(x, (x.shape[0], x.shape[1], 1))
+
+model = Sequential([
+ SimpleRNN(units=50, activation='relu', input_shape=(x.shape[1], 1)),
+ Dense(units=1)
+])
 model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train,y_train, batch_size=1, epochs=10)
+model.fit(x, y, epochs=50, batch_size=32)
+predictions = scaler.inverse_transform(model.predict(x))
 
-test_data= scaled_data[training_data_len-60:, :]
-
-x_test = []
-y_test = dataset[training_data_len:,:]
-for i in range(60,len(test_data)):
-    x_test.append(test_data[i-60:i,0])
-
-x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1],1))
-print(x_test.shape)
-
-predictions = model.predict(x_test)
-predictions = scaler.inverse_transform(predictions)
-
-rmse = np.sqrt(np.mean(predictions - y_test)**2)
-print(rmse)
-
-import warnings
-warnings.filterwarnings('ignore')
-
-
-train = data[:training_data_len]
-valid = data[training_data_len:]
-valid['Predictions'] = predictions
-
-plt.figure(figsize=(16,8))
-plt.title('Model')
-plt.xlabel('Date', fontsize=18)
-plt.ylabel('Close Price' ,fontsize=18)
-plt.plot(train['Close'],linewidth=3.5)
-plt.plot(valid[['Close','Predictions']],linewidth=3.5)
-plt.legend(['Train','Valid','Predictions'], loc='upper center')
+plt.figure(figsize=(14, 7))
+plt.plot(data.index[sequence_length:], data['Close'][sequence_length:], label='Actual Prices')
+plt.plot(data.index[sequence_length:], predictions, label='Predicted Prices')
+plt.title('Stock Price Prediction Using RNN')
+plt.xlabel('Date')
+plt.ylabel('Closing Price')
+plt.legend()
 plt.show()
-
-print(valid)
